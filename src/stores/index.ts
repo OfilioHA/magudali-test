b4bd3f3e49eedbf444c263ef4e9fe596242744e6
OfilioHA <herdocia20@gmail.com>
2024-04-28 13:39:41 -0600

    fix: :pencil2: Combining reducer for types

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,10 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import tasksReducer from './tasksSlice';
 
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+})
+
 const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-  },
+  reducer: rootReducer
 });
 
+export type RootState = ReturnType<typeof rootReducer>
 export default store;
