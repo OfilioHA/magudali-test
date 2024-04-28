c03381abef02eb790de63129bb20520a2e079b54
OfilioHA <herdocia20@gmail.com>
2024-04-27 22:01:16 -0600

    ci: :wrench: Jest config

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    ".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"  
+  },
+};
